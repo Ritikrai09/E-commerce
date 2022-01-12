96cfd01260188fca8a2f20e7e0502eda4b050db2
Ritikrai09 <62825561+Ritikrai09@users.noreply.github.com>
2022-01-12 13:24:43 +0530

    Add files via upload

--- ProductServiceProvider.php
+++ ProductServiceProvider.php
@@ -2,18 +2,19 @@
 
 namespace App\Providers;
 use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class ProductServiceProvider extends ServiceProvider
 {
-  
-        public function boot()
-        {
-            view()->composer('*',function($view) {
-               $view->with('product',Product::all());
-            
-        });
-    
-    }
+   
+    public function boot()
+    {   
+        View::composer(['layouts.ecommerce.productAvailable','layouts.ecommerce.product','layouts.ecommerce.bill'],function($view) {
+        $view->with('product', Category::where('seller_id', Auth::guard('seller')->user()->id)->with(['products'])->get());
+  });
+}
 }
     
